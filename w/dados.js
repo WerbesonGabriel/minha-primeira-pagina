420551c7cd46ce578620cd9ac1d1a1a725f20fda
WerbesonGabriel <rochawerbeson@gmail.com>
2024-09-06 22:01:35 -0300

    Add files via upload

---
+++ w/dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo:"neymar",
+        descricao:"neymar é um icone do futebol brasileiro",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+    },
+    {
+        titulo:"Jude Bellingham",
+        descricao:"Jude Bellingham é um ícone do futebol brasileiro",
+        link:"https://pt.wikipedia.org/wiki/Jude_Bellingham",
+},
+{
+    titulo:"péle",
+    descricao:"péle é o rei do futebol",
+    link:"https://pt.wikipedia.org/wiki/Pel%C3%A9",
+},
+
+];
